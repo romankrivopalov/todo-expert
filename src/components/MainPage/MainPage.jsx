96d2402e2676058db3f6311b435679f412669a65
Work desktop <the.red.lemon@yandex.ru>
2023-09-12 16:09:07 +0300

    feat: adds initial styles for main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,8 +1,27 @@
+import Container from '../Container/Container.jsx';
 import Header from '../Header/Header.jsx';
+import s from './MainPage.module.scss';
+import cn from 'classnames';
 
 const MainPage = ({ onSignout }) => (
   <>
     <Header onSignout={onSignout} />
+
+    <Container>
+      <form className={s.form}>
+        <input
+          className={s.input}
+          type='text'
+          placeholder='add task'
+        />
+        <div className={s.wrapper}>
+          <button className={s.btn}>today</button>
+          <button className={s.btn}>tomorrow</button>
+          <button className={cn(s.btn, s.btn_type_date)}></button>
+          <button className={cn(s.btn, s.btn_type_time)}></button>
+        </div>
+      </form>
+    </Container>
   </>
 )
 
