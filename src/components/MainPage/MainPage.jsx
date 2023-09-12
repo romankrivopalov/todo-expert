0d37e7fb3373fea3700d857e525510211d874ca9
Home <home@gmail.com>
2023-09-12 21:35:50 +0300

    feat: adds calendar

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,28 +1,144 @@
+import { useEffect, useState } from 'react';
 import Container from '../Container/Container.jsx';
 import Header from '../Header/Header.jsx';
 import s from './MainPage.module.scss';
 import cn from 'classnames';
+import dates from '../../utils/gateDate.js';
 
-const MainPage = ({ onSignout }) => (
-  <>
-    <Header onSignout={onSignout} />
+const MainPage = ({ onSignout }) => {
+  const [ values, setValues ] = useState({'task': ''});
+  const [ year, setYear ] = useState(null);
+  const [ month, setMonth ] = useState(null);
+  const [ today, setToday ] = useState(1);
+  const [ calendar, setCalendar ] = useState([]);
+  const [ isShowCalendar, setIsShowCalendar ] = useState(false);
 
-    <Container>
-      <form className={s.form}>
-        <input
-          className={s.input}
-          type='text'
-          placeholder='add task'
-        />
-        <div className={s.wrapper}>
-          <button className={s.btn}>today</button>
-          <button className={s.btn}>tomorrow</button>
-          <button className={cn(s.btn, s.btn_type_date)}></button>
-          <button className={cn(s.btn, s.btn_type_time)}></button>
-        </div>
-      </form>
-    </Container>
-  </>
-)
+  useEffect(() => {
+    setMonth(dates.getMonth());
+    setYear(dates.getYear());
+    setToday(dates.getNumDay());
+    setCalendar(dates.getAllDaysOfMonth());
+    console.log(today)
+  }, []);
+
+  const handleShowCalendar = () => {
+    setIsShowCalendar(!isShowCalendar);
+  }
+
+  const handleSetDay = () => {
+
+  }
+
+  const handleAddTask = () => {
+
+  }
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setValues((oldValues) =>({ ...oldValues, [name]: value }));
+  }
+
+  return (
+    <>
+      <Header onSignout={onSignout} />
+
+      <Container>
+        <form className={s.form}>
+          <input
+            className={s.input}
+            name="task"
+            type="text"
+            onChange={handleChange}
+            placeholder="add task"
+          />
+          <div className={s.container}>
+            <div className={s.wrapper}>
+              <button
+                className={cn(s.btn, s.btn_type_date)}
+                onClick={handleShowCalendar}
+                type="button" />
+              <button
+                className={cn(s.btn, s.btn_type_time)}
+                type="button" />
+              <div className={cn(s.date, isShowCalendar ? s.date_show : '')}>
+                <span className={s.month}>{month} {year}</span>
+                <div className={s.calendar}>
+                  <ul className={s.row}>
+                    <li className={s.week}>mo</li>
+                    <li className={s.week}>tu</li>
+                    <li className={s.week}>we</li>
+                    <li className={s.week}>th</li>
+                    <li className={s.week}>fr</li>
+                    <li className={s.week}>sa</li>
+                    <li className={s.week}>su</li>
+                  </ul>
+                  <ul className={s.row}>
+                    {calendar[0] && calendar[0].map(day =>
+                      <li
+                      key={day}
+                      className={cn(s.day, (day < today || day > 7) ? s.day_color_gray : '')}
+                      onClick={handleSetDay}
+                      >
+                        {day}
+                      </li>
+                    )}
+                  </ul>
+                  <ul className={s.row}>
+                    {calendar[1] && calendar[1].map(day =>
+                      <li
+                      key={day}
+                      className={cn(s.day, day < today ? s.day_color_gray : '')}
+                      onClick={handleSetDay}
+                      >
+                        {day}
+                      </li>
+                    )}
+                  </ul>
+                  <ul className={s.row}>
+                    {calendar[2] && calendar[2].map(day =>
+                      <li
+                      key={day}
+                      className={cn(s.day, day < today ? s.day_color_gray : '')}
+                      onClick={handleSetDay}
+                      >
+                        {day}
+                      </li>
+                    )}
+                  </ul>
+                  <ul className={s.row}>
+                    {calendar[3] && calendar[3].map(day =>
+                      <li
+                      key={day}
+                      className={cn(s.day, day < today ? s.day_color_gray : '')}
+                      onClick={handleSetDay}
+                      >
+                        {day}
+                      </li>
+                    )}
+                  </ul>
+                  <ul className={s.row}>
+                    {calendar[4] && calendar[4].map(day =>
+                      <li
+                      key={day}
+                      className={cn(s.day, (day < today && day > 7) ? s.day_color_gray : '')}
+                      onClick={handleSetDay}
+                      >
+                        {day}
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <button className={cn(s.btn, s.btn_type_submit)}>
+              Add
+            </button>
+          </div>
+        </form>
+      </Container>
+    </>
+  )
+}
 
 export default MainPage
---
+++ src/utils/gateDate.js
@@ -0,0 +1,96 @@
+class Dates {
+  now = new Date();
+  months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  getNumDay = () => {
+    return this.now.getDate();
+  }
+
+  getYear = () => {
+    return this.now.getFullYear();
+  }
+
+  getMonth = () => {
+    return this.months[this.now.getMonth()];
+  }
+
+  // метод возвращает массив дат по неделям
+  getAllDaysOfMonth = () => {
+    // номер дня недели первого числа месяца
+    const numFirstDayMonthOfWeek = new Date(this.getYear(), this.now.getMonth(), 1).getDay()
+    // количество дней в прошлом месяце
+    const numDayOfLastMonth = new Date(this.getYear(), this.now.getMonth(), 0).getDate();
+    const numDayOfMonth = new Date(this.getYear(), this.now.getMonth() + 1, 0).getDate();
+    const firstWeek = [];
+    const secondWeek = [];
+    const thirdWeek = [];
+    const fourthWeek = [];
+    const fifthWeek = [];
+
+    let countA = 0;
+    let countB = 1;
+    let countC = 1;
+
+    // первая неделя
+    for (let i = 0; i < 7; i++) {
+      // если первый день месяца понедельник
+      if (numFirstDayMonthOfWeek === 1) {
+        firstWeek.push(i + 1);
+      } else {
+        // из общего количества дней вычитаем номер дня недели и добавляем счётчик
+        // чтобы определить, когда начинается новый месяц, чтобы добавить их в массив
+        const res = numDayOfLastMonth - (numFirstDayMonthOfWeek - 2) + countA;
+
+        // если результат больше, добавляем второй счётчик с 1 числа
+        if (res > numDayOfLastMonth) {
+          firstWeek.push(countB);
+
+          countB++
+        }
+
+        // если результат меньше
+        if (res <= numDayOfLastMonth) {
+          firstWeek.push(res);
+
+          countA++
+        }
+      }
+    }
+
+    for (let i = firstWeek[6]; i < firstWeek[6] + 7; i++) secondWeek.push(i + 1);
+    for (let i = secondWeek[6]; i < secondWeek[6] + 7; i++) thirdWeek.push(i + 1);
+    for (let i = thirdWeek[6]; i < thirdWeek[6] + 7; i++) fourthWeek.push(i + 1);
+
+    // последняя неделя
+    for (let i = fourthWeek[6]; i < numDayOfLastMonth; i++) {
+      let res = numDayOfMonth - i;
+
+      if (res > 0) {
+        fifthWeek.push(i + 1);
+      } else {
+        fifthWeek.push(countC);
+
+        countC++
+      }
+    }
+
+    return [ firstWeek, secondWeek, thirdWeek, fourthWeek, fifthWeek ]
+  }
+}
+
+const dates = new Dates()
+
+export default dates
